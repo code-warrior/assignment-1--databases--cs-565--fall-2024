bea99e5c617ce6bddd40be518f3a930ea9083f5d
Roy Vanegas <roy@thecodeeducators.com>
2024-09-30 11:27:01 -0400

    Add commands.sql

---
+++ commands.sql
@@ -0,0 +1,7 @@
+-- Get the the spice name, store, and URL of spices
+
+-- Get the the spice name, barcode, store, and URL of spices
+
+-- Get stores who only have an online address
+
+-- Get stores who have an online and a physical address
