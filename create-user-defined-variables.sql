9c713d13062cba1b553f002fe9db52681ca85d45
Roy Vanegas <roy@thecodeeducators.com>
2024-09-30 09:44:29 -0400

    Add create-user-defined-variables.sql

---
+++ create-user-defined-variables.sql
@@ -0,0 +1,26 @@
+-- The following session variables are all for spice brands
+
+SET @BADIA = 'Badia';
+SET @BRAGG = 'Bragg';
+SET @COSTCO = 'Costco';
+SET @DAVIS = 'Davis';
+SET @DROGHERIA_ALIMENTARI = 'Drogheria &amp; Alimentari';   -- Note that &amp; is an HTML entity for the ampersand, &, character
+SET @FAIRWAY = 'Fairway';
+SET @FRESH_DIRECT = 'Fresh Direct';
+SET @FRONTIER_CO_OP = 'Frontier Co-op';
+SET @FRONTIER = 'Frontier Culinary Spices';
+SET @GUSTUS_VITAE = 'Gustus Vitae';
+SET @KAMMENSTEIN = 'M. Kammenstein';
+SET @KEY_FOOD = 'Key Food';
+SET @LA_FLOR = 'La Flor';
+SET @MCCORMICK = 'McCormick';
+SET @MCCORMICK_GOURMET = 'McCormick — Gourmet Collection';    -- Note that the em dash, —, is hugged by hair spaces
+SET @MIMIS_PRODUCTS = 'Mimi’s Products';                    -- Note that the proper apostrophe, or right single typographer’s quote, is used here
+SET @MORTON_BASSETT = 'Morton &amp; Bassett';
+SET @NIELSON_MASSEY = 'Nielson-Massey Vanillas';
+SET @SIMPLY_ORGANIC = 'Simply Organic';
+SET @SPICE_HUNTER = 'The Spice Hunter';
+SET @SPICE_LAB = 'The Spice Lab';
+SET @SPICE_SUPREME = 'Spice Supreme';
+SET @TRADER_JOES = 'Trader Joe’s';
+SET @WHOLE_FOODS_365 = 'Whole Foods 365';
