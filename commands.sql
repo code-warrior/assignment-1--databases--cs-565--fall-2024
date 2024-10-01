d2309418e9844cc161ff9d1f3efafe87b15aa1b6
Roy Vanegas <roy@thecodeeducators.com>
2024-09-30 20:36:25 -0400

    Update wording so they’re clearer

--- commands.sql
+++ commands.sql
@@ -1,6 +1,6 @@
--- Get the the spice name, store, and URL of spices
+-- Get the name of the store and its URL where any spices were purchased.
 
--- Get the the spice name, barcode, store, and URL of spices
+-- Get the name of the store and its URL where spices were purchased, including their barcode.
 
 -- Get stores who only have an online address
 
