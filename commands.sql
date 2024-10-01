33c62237085b4a89604d9f047c968dbb2ea7295d
Roy Vanegas <roy@thecodeeducators.com>
2024-09-30 20:37:19 -0400

    Add remaining command requests

--- commands.sql
+++ commands.sql
@@ -5,3 +5,9 @@
 -- Get stores who only have an online address.
 
 -- Get stores who have an online and a physical address.
+
+-- Get spices whose names are also brands. For example, Fresh Direct is a store that also makes spices.
+
+-- Get spices whose names contain “cinnamon”.
+
+-- Get the brand name of the Sumac spice and the name and URL of the store where it was purchased.
