3e514d53275831ed2b81f932045ec5a28decc41c
Roy Vanegas <roy@thecodeeducators.com>
2024-09-30 20:36:41 -0400

    Add a period at the end of the sentence

--- commands.sql
+++ commands.sql
@@ -2,6 +2,6 @@
 
 -- Get the name of the store and its URL where spices were purchased, including their barcode.
 
--- Get stores who only have an online address
+-- Get stores who only have an online address.
 
--- Get stores who have an online and a physical address
+-- Get stores who have an online and a physical address.
